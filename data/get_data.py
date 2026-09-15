c3b8e7c17af3df80dfc8d8b01475705e6758477e
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2026-09-15 15:04:32 +0600

    update get_data.py

--- data/get_data.py
+++ data/get_data.py
@@ -4,11 +4,11 @@ import os
 # get roboflow api key
 ROBOFLOW_KEY = os.getenv("ROBOFLOW_KEY")
 
-if not ROBOFLOW_KEY:
-    raise ValueError("Api Key must be set as ROBOFLOW_KEY")
-
 def get_data_roboflow(user_id, project_id, version=1):
     """ Get coco dataset from Roboflow """
+    if not ROBOFLOW_KEY:
+        raise ValueError("Api Key must be set as ROBOFLOW_KEY")
+
     rf = Roboflow(api_key=ROBOFLOW_KEY)
     project = rf.workspace(user_id).project(project_id)
     version = project.version(version)
